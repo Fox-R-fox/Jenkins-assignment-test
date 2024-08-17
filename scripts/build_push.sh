bff293ba79061432946c5c6db0b4bb94ba838a2b
Fox-R-fox <rohansherkar2207@gmail.com>
2024-08-17 20:43:13 +0530

    Update build_push.sh

--- scripts/build_push.sh
+++ scripts/build_push.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-# Variables
-IMAGE_NAME="yourdockerhubusername/your-app-image:latest"
-DOCKERHUB_USERNAME="yourdockerhubusername"
+# Retrieve Docker Hub credentials from AWS Secrets Manager
+SECRET_JSON=$(aws secretsmanager get-secret-value --secret-id arn:aws:secretsmanager:us-east-1:339712721384:secret:docker-us-JYDQoe --query SecretString --output text)
+DOCKERHUB_USERNAME=$(echo $SECRET_JSON | jq -r '.dockerhub_username')
+DOCKERHUB_PASSWORD=$(echo $SECRET_JSON | jq -r '.dockerhub_password')
 
 # Login to Docker Hub
 echo "Logging into Docker Hub..."
-docker login -u "$DOCKERHUB_USERNAME" -p "$DOCKERHUB_PASSWORD"
+echo "$DOCKERHUB_PASSWORD" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
 
 # Build Docker Image
 echo "Building Docker image..."
-docker build -t "$IMAGE_NAME" .
+docker build -t "$DOCKERHUB_USERNAME/your-app-image:latest" .
 
 # Push Docker Image to Docker Hub
 echo "Pushing Docker image to Docker Hub..."
-docker push "$IMAGE_NAME"
+docker push "$DOCKERHUB_USERNAME/your-app-image:latest"
