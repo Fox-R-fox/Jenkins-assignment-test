bac6650434ae4b1189a2c87523f831f686cfffa9
Fox-R-fox <rohansherkar2207@gmail.com>
2024-08-18 20:39:16 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,33 +2,21 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_eks_cluster" "game_library_cluster" {
-  name     = "game-library-cluster"
-  role_arn = aws_iam_role.eks_cluster_role.arn
-
-  vpc_config {
-    subnet_ids = aws_subnet.private_subnets[*].id
-  }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.eks_cluster_policy,
-    aws_iam_role_policy_attachment.eks_vpc_resource_controller
-  ]
+# VPC definition
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_eks_node_group" "game_library_nodes" {
-  cluster_name    = aws_eks_cluster.game_library_cluster.name
-  node_group_name = "game-library-nodes"
-  node_role_arn   = aws_iam_role.eks_worker_role.arn
-  subnet_ids      = aws_subnet.private_subnets[*].id
-
-  scaling_config {
-    desired_size = 2
-    min_size     = 1
-    max_size     = 3
-  }
+# Subnets for the EKS cluster
+resource "aws_subnet" "private_subnets" {
+  count                   = 2
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index + 1)
+  availability_zone       = element(data.aws_availability_zones.available.names, count.index)
+  map_public_ip_on_launch = false
 }
 
+# EKS Cluster IAM Role
 resource "aws_iam_role" "eks_cluster_role" {
   name = "eks-cluster-role"
 
@@ -46,6 +34,7 @@ resource "aws_iam_role" "eks_cluster_role" {
   })
 }
 
+# EKS Worker Node IAM Role
 resource "aws_iam_role" "eks_worker_role" {
   name = "eks-worker-role"
 
@@ -61,4 +50,72 @@ resource "aws_iam_role" "eks_worker_role" {
       }
     ]
   })
+}
+
+# IAM Policy Attachments for EKS Cluster Role
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_vpc_resource_controller" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+}
+
+# IAM Policy Attachments for EKS Worker Role
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+  role       = aws_iam_role.eks_worker_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  role       = aws_iam_role.eks_worker_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSCNIPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_container_registry_readonly" {
+  role       = aws_iam_role.eks_worker_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+# EKS Cluster Definition
+resource "aws_eks_cluster" "game_library_cluster" {
+  name     = "game-library-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = aws_subnet.private_subnets[*].id
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy,
+    aws_iam_role_policy_attachment.eks_vpc_resource_controller
+  ]
+}
+
+# EKS Node Group Definition
+resource "aws_eks_node_group" "game_library_nodes" {
+  cluster_name    = aws_eks_cluster.game_library_cluster.name
+  node_group_name = "game-library-nodes"
+  node_role_arn   = aws_iam_role.eks_worker_role.arn
+  subnet_ids      = aws_subnet.private_subnets[*].id
+
+  scaling_config {
+    desired_size = 2
+    min_size     = 1
+    max_size     = 3
+  }
+
+  depends_on = [
+    aws_eks_cluster.game_library_cluster,
+    aws_iam_role_policy_attachment.eks_worker_node_policy,
+    aws_iam_role_policy_attachment.eks_cni_policy,
+    aws_iam_role_policy_attachment.ec2_container_registry_readonly
+  ]
+}
+
+# Data sources to get AWS availability zones
+data "aws_availability_zones" "available" {
+  state = "available"
 }
