d3a573747cf450d531dfc864784f1ffc400b1296
Fox-R-fox <rohansherkar2207@gmail.com>
2024-08-18 21:21:58 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,15 +5,50 @@ provider "aws" {
 # VPC definition
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "main-vpc"
+  }
 }
 
-# Subnets for the EKS cluster
+# Subnets for the EKS cluster (private)
 resource "aws_subnet" "private_subnets" {
   count                   = 2
   vpc_id                  = aws_vpc.main.id
   cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index + 1)
   availability_zone       = element(data.aws_availability_zones.available.names, count.index)
   map_public_ip_on_launch = false
+
+  tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/game-library-cluster" = "shared"
+    Name = "private-subnet-${count.index}"
+  }
+}
+
+# Security group for EKS worker nodes
+resource "aws_security_group" "eks_worker_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "eks-worker-sg"
+  }
 }
 
 # EKS Cluster IAM Role
@@ -86,6 +121,7 @@ resource "aws_eks_cluster" "game_library_cluster" {
 
   vpc_config {
     subnet_ids = aws_subnet.private_subnets[*].id
+    security_group_ids = [aws_security_group.eks_worker_sg.id]
   }
 
   depends_on = [
